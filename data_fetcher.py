b8995fe8f9f4f8cdaf68b3f6751e3284fdf42e94
schamja Shahaga <schamja@gmx.de>
2026-01-22 18:14:19 +0100

    Security: moving API key to .env and adding gitignore

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,10 +1,26 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-API_KEY = 'tJcOUz1ik7BJqpzaYiw8LwffYWtNdOoYLzwuHav7'
+# Lädt die Variablen aus der .env Datei in das System
+load_dotenv()
 
-def fetch_data(animal_name): # <--- Prüfe diesen Namen!
-    api_url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
-    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+# Greift auf die Variable zu
+API_KEY = os.getenv('API_KEY')
+API_URL = 'https://api.api-ninjas.com/v1/animals?name={}'
+
+
+def fetch_data(animal_name):
+    # Falls der Key nicht gefunden wurde, geben wir eine Warnung aus
+    if not API_KEY:
+        print("Fehler: API_KEY wurde nicht in der .env Datei gefunden!")
+        return []
+
+    response = requests.get(
+        API_URL.format(animal_name),
+        headers={'X-Api-Key': API_KEY}
+    )
+
     if response.status_code == 200:
         return response.json()
     return []
